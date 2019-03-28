870c40c8022018b219cc2aeb40680626d2dadcf2
weidy2 <dylan.y.q.wei@oocl.com>
2019-03-28 20:47:31 +0800

    added spring security

---
+++ springcloud-security/src/main/java/com/oocl/sprintsecurity/SprintSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.oocl.sprintsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintSecurityApplication.class, args);
+	}
+
+}
