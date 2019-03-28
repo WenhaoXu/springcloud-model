627bce950c9b73028b0813e3660f0699a322a004
weidy2 <dylan.y.q.wei@oocl.com>
2019-03-28 23:09:13 +0800

    configured the spring security and make todolist service a protected service.

--- springcloud-security/src/test/java/com/oocl/springsecurity/SpringSecurityApplicationTests.java
+++ springcloud-security/src/test/java/com/oocl/springsecurity/SpringSecurityApplicationTests.java
@@ -1,4 +1,4 @@
-package com.oocl.sprintsecurity;
+package com.oocl.springsecurity;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SprintSecurityApplicationTests {
+public class SpringSecurityApplicationTests {
 
 	@Test
 	public void contextLoads() {
